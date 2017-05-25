d33bf75102eeca4b7b36813b082730242e82904e
Jakob Werner <jawerne@microsoft.com>
2017-05-25 14:27:21 +0200

    Added some basic tests, changed the configuration, updated ava

--- test.js
+++ test.js
@@ -1,23 +1,90 @@
 /* eslint max-len:0 */
 import postcss from 'postcss';
 import test    from 'ava';
-
 import plugin from './';
-
-function run(t, input, output, opts = { }) {
-    return postcss([ plugin(opts) ]).process(input)
-        .then( result => {
-            t.same(result.css, output);
-            t.same(result.warnings().length, 0);
-        });
-}
 
 /* Write tests here */
 
 test('Checks initial configuration', t => {
-    return run(t, 'a{ color: red }', 'a{ color: red }', { });
+    const input = 'a{ color: red }';
+    const output = 'a{ color: red }';
+
+    return postcss([plugin()]).process(input)
+        .then( result => {
+            t.is(result.css, output);
+        });
 });
 
 test('Supports rtlcss control directives', t => {
-    return run(t, 'a{ /*rtl:ignore*/left: 0 }', 'a{ /*rtl:ignore*/left: 0 }', { });
+    const input = 'a{ /*rtl:ignore*/left: 0 }';
+    const output = 'a{ /*rtl:ignore*/left: 0 }';
+
+    return postcss([plugin()]).process(input)
+        .then( result => {
+            t.is(result.css, output);
+        });
+});
+
+test('Checks basic example', t => {
+    const input = `
+a { 
+    float: left 
+}`;
+
+    const output = `
+html[dir="ltr"] a { 
+    float: left
+}
+html[dir="rtl"] a { 
+    float: right
+}`;
+    return postcss([plugin()]).process(input)
+        .then( result => {
+            t.is(result.css, output);
+        });
+});
+
+test('Checks example with two properties which should both be converted', t => {
+    const input = `
+a { 
+    float: left;
+    transform: translateX(50%);
+}`;
+
+    const output = `
+html[dir="ltr"] a { 
+    float: left;
+    transform: translateX(50%)
+}
+html[dir="rtl"] a { 
+    float: right;
+    transform: translateX(-50%)
+}`;
+    return postcss([plugin()]).process(input)
+        .then( result => {
+            t.is(result.css, output);
+        });
+});
+
+test('Checks example with two properties out of which only one should be converted', t => {
+    const input = `
+a {
+    color: red;
+    transform: translateX(50%);
+}`;
+
+    const output = `
+a {
+    color: red;
+}
+html[dir="ltr"] a {
+    transform: translateX(50%);
+}
+html[dir="rtl"] a {
+    transform: translateX(-50%);
+}`;
+    return postcss([plugin()]).process(input)
+        .then( result => {
+            t.is(result.css, output);
+        });
 });
